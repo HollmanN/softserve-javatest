ba5a353c2b4741005f32b54802c674957aba8fff
Hollman N Castro <hollman_castro@outlook.com>
2024-01-21 20:12:56 -0500

    :white_check_mark: Added base test cases for ServiceParserService based on TDD

---
+++ src/main/java/com/softserve/javatest/service/StringParserService.java
@@ -0,0 +1,10 @@
+package com.softserve.javatest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StringParserService {
+    public String parseString(String parseString){
+        return "";
+    }
+}
---
+++ src/test/java/com/softserve/javatest/service/StringParserServiceTest.java
@@ -0,0 +1,32 @@
+package com.softserve.javatest.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+public class StringParserServiceTest {
+    @InjectMocks
+    StringParserService stringParserService;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void whenStringHasDuplicates_thenRemoveDuplicates(){
+        String input = "AAAABBBBBCCCCCDEEEE";
+        String expected = "ABCDE";
+        String result = stringParserService.parseString(input);
+        assert result.equals(expected);
+    }
+
+    @Test
+    public void whenStringDoesntHasDuplicates_thenReturnSameString(){
+        String input = "ABCDE";
+        String expected = "ABCDE";
+        String result = stringParserService.parseString(input);
+        assert result.equals(expected);
+    }
+}
