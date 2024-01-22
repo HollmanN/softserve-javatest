510a50e2437c4cc9ad43af6629758e3337c6334e
Hollman N Castro <hollman_castro@outlook.com>
2024-01-21 22:08:52 -0500

    ":white_check_mark: Added service bean in controller layer"

--- src/main/java/com/softserve/javatest/controller/StringParserController.java
+++ src/main/java/com/softserve/javatest/controller/StringParserController.java
@@ -1,6 +1,7 @@
 package com.softserve.javatest.controller;
 
 import com.softserve.javatest.model.StringParserRecord;
+import com.softserve.javatest.service.StringParserService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringParserController {
+    private final StringParserService stringParserService;
+
+    public StringParserController(StringParserService stringParserService){
+        this.stringParserService = stringParserService;
+    }
+
     @PostMapping("/duplicates")
     public String removeDuplicates(@Validated @RequestBody StringParserRecord input){
-        return "";
+        return stringParserService.removeDuplicatesFromString(input.input());
     }
 }
